cc2abde24932089146c4fe7776c6eeaad083ff97
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-02 01:47:39 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
       ];
 
       perSystem =
-        { system, pkgs, self, lib, ... }:
+        { system, pkgs, self', lib, ... }:
         let
           nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
