d1fcabe5cac52b1f076437129311de41f9709ec0
TeoMAraujo <teoaraujoufmg@gmail.com>
2025-04-02 01:57:59 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,58 +1,62 @@
 {
-  description = "A nixvim configuration";
+  description = "xd";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
-     pre-commit-hooks = {
+    };
+    pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-      
     };
   };
 
   outputs =
-    { nixpkgs, nixvim, flake-parts, pre-commit-hooks,... }@inputs:
+    {nixpkgs,nixvim,flake-parts,pre-commit-hooks, ...}@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
+        "x86_64-linux"
         "aarch64-darwin"
+        "x86_64-darwin"
       ];
 
       perSystem =
-        { system, ... }:
+        {system,pkgs,self',lib,...}:
         let
-          nixvimLib = nixvim.lib.${system};
           nixvim' = nixvim.legacyPackages.${system};
           nixvimModule = {
-            inherit pkgs; # or alternatively, set `pkgs`
-            module = import ./config; # import the module directly
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            inherit pkgs;
+            module = import ./config;
             extraSpecialArgs = {
-                inherit inputs system;
+              inherit inputs system;
             };
           };
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
           checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+            pre-commit-check = pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                statix.enable = true;
+                nixfmt-rfc-style.enable = true;
+              };
+            };
           };
+
+          formatter = pkgs.nixfmt-rfc-style;
 
           packages.default = nvim;
 
-           devShells.default =
+          devShells.default =
             with pkgs;
             mkShell {
               inherit (self'.checks.pre-commit-check) shellHook;
-          };
+            };
         };
     };
 }
